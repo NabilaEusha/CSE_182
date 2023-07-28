0044583dabf1a1d07ea45d2a866e251c91d50022
Nabila Sultana Eusha <86979752+NabilaEusha@users.noreply.github.com>
2023-07-28 11:17:47 +0600

    Add files via upload

---
+++ lab_exp_04/prac_02.c
@@ -0,0 +1,14 @@
+//Write a C program to print all alphabets from a to z.-using while loop. 
+#include <stdio.h>
+
+int main(){
+    char ch;
+
+    printf("Alphabets from a - z are: \n");
+    for (ch='a' ; ch <='z'; ch++)
+    {
+       printf("%c\n",ch);
+    }
+    
+    return 0;
+}
